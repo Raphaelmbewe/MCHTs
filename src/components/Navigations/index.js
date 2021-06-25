8e2cd826ab02120f848a10cd6eeca6e2b3248242
Raphael Limbikani Mbewe <rmbewe68@gmail.com>
2021-06-25 08:09:11 +0100

    made chages

--- src/components/Navigations/index.js
+++ src/components/Navigations/index.js
@@ -1,40 +1,42 @@
 import React from 'react';
 import './navigation.css'
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav , Container} from 'react-bootstrap';
 import Logo from '../../images/logo.png'
 export const Navigation = ()=>{
 
     return (
       <div>
-        <Navbar expand='lg' className='fixed-top nav-background '>
-          <Navbar.Brand href='/'>
-            <div className='logoAndName'>
-              <img src={Logo} alt='' className=' align-top img-fluid logo' />
-              <div className='nameTag'>
-                <div className='nameFP'> My Community </div>
-                Health
+        <Container container-fluid>
+          <Navbar expand='lg' className='fixed-top nav-background '>
+            <Navbar.Brand href='/'>
+              <div className='logoAndName'>
+                <img src={Logo} alt='' className=' align-top img-fluid logo' />
+                <div className='nameTag'>
+                  <div className='nameFP'> My Community </div>
+                  Health
+                </div>
               </div>
-            </div>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='mr-auto my-nav-items  text-secondary '>
-              <Nav.Link href='/' className='text-dark'>
-                Home
-              </Nav.Link>
-              <Nav.Link href='/services' className='text-secondary'>
-                Services
-              </Nav.Link>
-              <Nav.Link href='/about' className='text-secondary'>
-                About Us
-              </Nav.Link>
-              <Nav.Link href='/contact' className='text-secondary'>
-                Contact
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            <Navbar.Collapse id='basic-navbar-nav'>
+              <Nav className='mr-auto my-nav-items  text-secondary '>
+                <Nav.Link href='/' className='text-dark'>
+                  Home
+                </Nav.Link>
+                <Nav.Link href='/services' className='text-secondary'>
+                  Services
+                </Nav.Link>
+                <Nav.Link href='/about' className='text-secondary'>
+                  About Us
+                </Nav.Link>
+                <Nav.Link href='/contact' className='text-secondary'>
+                  Contact
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Container>
       </div>
     );
 }
